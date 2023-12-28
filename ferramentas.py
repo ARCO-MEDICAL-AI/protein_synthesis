452c79d079f18d0efdd8d38536952f0cd4067a07
LT <153869588+ARCO-MEDICAL-AI@users.noreply.github.com>
2023-12-28 20:37:22 -0300

    ferramentas.py

--- ferramentas.py
+++ ferramentas.py
@@ -41,14 +41,3 @@ class MonomericProteins:
         return codon_string
 
     
-
-# Test the class
-mp = MonomericProteins()
-
-# Give one protein to be synthesized  e its aminoacids
-test_protein = mp.metabolic_demand()
-
-
-# Create a variable to show the codon sequence nedeed to synthesize the given protein 
-test_codon_string = mp.create_codon_string(test_protein)
-print(test_codon_string)
