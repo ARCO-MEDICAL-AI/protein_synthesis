aa01ccc04b37c4f4c4a88e4aa5607c1e4a4f0d15
LT <153869588+ARCO-MEDICAL-AI@users.noreply.github.com>
2023-12-29 12:08:24 -0300

    # _main_.py

--- # _main_.py
+++ # _main_.py
@@ -1,19 +1,11 @@
 # _main_
 
 from ferramentas import *
-# Test the class
-mp = MonomericProteins()
-
-# Give one protein to be synthesized  e its aminoacids
-test_protein = mp.metabolic_demand()
 
 
-# Create a variable to show the codon sequence nedeed to synthesize the given protein 
-test_codon_string = mp.create_codon_string(test_protein)
-print(test_codon_string)
 
-# instantiate the MonomericProteins class
+# First, instantiate the MonomericProteins class
 protein_synthesizer = MonomericProteins()
 
-# use the object to call the method
-protein_synthesizer.synthesize_multiple_proteins(5)
+# Now, use the object to call the method
+protein_synthesizer.synthesize_multiple_proteins(3)
