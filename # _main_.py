bc5b6d4a535f0001316f185b6f4278bbc5dac722
LT <153869588+ARCO-MEDICAL-AI@users.noreply.github.com>
2023-12-29 11:21:52 -0300

    # _main_.py

--- # _main_.py
+++ # _main_.py
@@ -11,3 +11,9 @@ test_protein = mp.metabolic_demand()
 # Create a variable to show the codon sequence nedeed to synthesize the given protein 
 test_codon_string = mp.create_codon_string(test_protein)
 print(test_codon_string)
+
+# instantiate the MonomericProteins class
+protein_synthesizer = MonomericProteins()
+
+# use the object to call the method
+protein_synthesizer.synthesize_multiple_proteins(5)
